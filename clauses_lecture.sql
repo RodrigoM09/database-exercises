c3515a224e761556e454171089b8484107616513
Rodrigo Marquez <rodrigo.nmn.marquez@gmail.com>
2022-11-08 14:26:53 -0600

    limit

--- clauses_lecture.sql
+++ clauses_lecture.sql
@@ -41,4 +41,16 @@ FROM employees
 WHERE birth_date NOT LIKE '195%'
 AND hire_date NOT LIKE '199%';
 
+SELECT *
+FROM employees
+WHERE birth_date
+LIKE '1954-01%'
+ORDER BY birth_date, last_name
+LIMIT 50;
 
+SELECT *
+FROM employees
+WHERE birth_date
+LIKE '1954-01%'
+ORDER BY birth_date, last_name
+LIMIT 50 OFFSET 50;
--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -3,15 +3,18 @@ USE employees;
 SELECT first_name, gender
 FROM employees
 WHERE (first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya')
-AND gender = 'M';
+AND gender = 'M'
+ORDER BY first_name ASC;
 
 SELECT *
 FROM employees
-WHERE last_name REGEXP '^E';
+WHERE last_name REGEXP '^E'
+ORDER BY emp_no ASC;
 
 SELECT *
 FROM employees
-WHERE last_name REGEXP 'q$';
+WHERE last_name REGEXP 'q$'
+ORDER BY emp_no DESC;
 
 SELECT *
 FROM employees
@@ -21,6 +24,3 @@ SELECT *
 FROM employees
 WHERE last_name
 LIKE '%q%' AND last_name NOT LIKE '%qu%';
-
-
-
